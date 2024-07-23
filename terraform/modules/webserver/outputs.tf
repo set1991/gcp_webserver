9576f0634cb0bccc5a1645727723424ec2047c1a
Alexandr Aliferenko <set1991@tut.by>
2024-07-23 09:45:44 +0300

    changes

--- terraform/modules/webserver/outputs.tf
+++ terraform/modules/webserver/outputs.tf
@@ -1,4 +1,4 @@
-#  Output link backend service to module load_balancer.
+#  Output ip address to nginx webserver
 
 output "ip_webserver" {
   value =  google_compute_instance.webserver.network_interface.0.access_config.0.nat_ip
