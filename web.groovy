cfed8d9c3a13894b6f158ff303880df88d8319ed
Alexandr Aliferenko <set1991@tut.by>
2024-07-23 08:07:36 +0300

    add changes

--- web.groovy
+++ web.groovy
@@ -8,15 +8,6 @@ pipeline {
         ANSIBLE_PATH = 'ansible_project/*'  // Путь к Ansible файлам
         }*/
     stages {
-       /* stage ('GIT clone') {
-            agent { label 'master' }
-            steps {
-           // The below will clone your repo and will be checked out to master branch by default.
-            git credentialsId: 'jenkins-rsa', url: 'git@github.com:set1991/gcp_webserver.git'
-           // Do a ls -lart to view all the files are cloned. It will be clonned. This is just for you to be sure about it.
-            sh "ls -lart ./*"         
-            }
-        }*/
        
        /* stage('Check Changes') {
             steps {
